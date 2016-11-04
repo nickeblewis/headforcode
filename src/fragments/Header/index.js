7654d41e8adb986a7c06ad3ba44b7b2f0f82e1a7
DESKTOP-O5P82QB\nicke <nickeblewis@gmail.com>
2016-11-04 08:24:34 +0000

    Temporarily removed blog from nav not ready

--- src/fragments/Header/index.js
+++ src/fragments/Header/index.js
@@ -65,11 +65,6 @@ export default class Header extends Component {
                   </Link>
                 </li>
                 <li className={styles.navItem}>
-                  <Link to='/posts' className={styles.link}>
-                    Blog
-                  </Link>
-                </li>
-                <li className={styles.navItem}>
                   <Link to='/courses' className={styles.link}>
                     Courses
                   </Link>
