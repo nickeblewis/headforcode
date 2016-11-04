7d170d37708f028b7d241a3a4a0e139043cc7f2c
DESKTOP-O5P82QB\nicke <nickeblewis@gmail.com>
2016-11-04 08:26:02 +0000

    Struggling with adsense

--- src/fragments/BetaCTA/index.js
+++ src/fragments/BetaCTA/index.js
@@ -5,7 +5,7 @@ import platformImage from '../../assets/images/platform_4.gif'
 import Block from '../../components/Block'
 import Button from '../../components/Button'
 import styles from './BetaCTA.css'
-import AdSense from 'react-adsense'
+// import GoogleAd from 'react-google-ad'
 
 const BetaCTA = (props) => {
   const url = 'https://serverless.com/framework/'
@@ -25,9 +25,8 @@ const BetaCTA = (props) => {
     >
       <Block className={styles.container}>
         <h1 className={styles.title}>We shall chuck a Google Adwords ad in here!</h1>
-
       </Block>
-      <AdSense.Google client='ca-pub-7074532723193891' slot='7960527967' />
+
     </NewAuth>
   )
 }
