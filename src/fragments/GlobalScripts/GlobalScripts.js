8be73478cb163aa2048af8c5bd2e025144846330
Nick Lewis <nickeblewis@gmail.com>
2016-11-30 14:56:20 +0000

    updates to comments and started to add drip

--- src/fragments/GlobalScripts/GlobalScripts.js
+++ src/fragments/GlobalScripts/GlobalScripts.js
@@ -2,10 +2,12 @@ import React, {PropTypes} from 'react'
 import GoogleAnalytics from './GoogleAnalytics'
 import GoogleAdsense from './GoogleAdsense'
 import SegmentIO from './SegmentIO'
+import DripIO from './DripIO'
 
 export default function GlobalScripts (props) {
   return (
     <div>
+      <DripIO />
       <GoogleAnalytics {...props} />
       <div id='footerScripts'
         dangerouslySetInnerHTML={{__html: SegmentIO}}
