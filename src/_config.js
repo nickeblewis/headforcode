d13703df6d99ad9f778f693e40673a5cabe2b5b3
DESKTOP-O5P82QB\nicke <nickeblewis@gmail.com>
2016-12-31 09:15:05 +0100

    Changed our Analytics tracking ID due to Russian hack

--- src/_config.js
+++ src/_config.js
@@ -13,15 +13,15 @@ module.exports = function siteConfig (isProduction) {
       SITENAME: process.env.SITENAME || 'headforcode',
       TWITTER: process.env.TWITTER || 'headforcode',
       GITHUB: process.env.GITHUB || 'nickeblewis',
-      GOOGLE_ANALYTICS_UA: (isProduction) ? 'UA-86079311-1' : null,
+      GOOGLE_ANALYTICS_UA: (isProduction) ? 'UA-86079311-2' : null,
       GOOGLE_ADSENSE_CLIENT: process.env.GOOGLE_ADSENSE_CLIENT || 'ca-pub-7074532723193891',
       GOOGLE_ADSENSE_CLIENT: process.env.GOOGLE_ADSENSE_SLOT || '7960527967',
       AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || 'eYJPvZumARLthBXJgMpeHsOXx9DvA42Y',
       AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || 'headforcode.eu.auth0.com',
       ALGOLIA_ADMIN: process.env.ALGOLIA_ADMIN || 'db43bbe2cb26bfa46fe72a5cd2c26213',
       API: {
-        ERROR: 'https://h413evrxuk.execute-api.us-west-2.amazonaws.com/dev/report',
-        NEWSLETTER: 'https://3upqirwiuc.execute-api.us-west-2.amazonaws.com/dev/subscribe'
+        ERROR: 'https://headforcode.com/api',
+        NEWSLETTER: 'https://headforcode.com/api'
       }
     }
   )
